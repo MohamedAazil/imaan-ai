c02e1fe8e4e30a9d2cd9aa3f9e488b7037201ab1
MohamedAazil <mohamedaazil1106@gmail.com>
2025-03-01 19:42:02 +0530

    colour to salam

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center h-16 px-6">
-      <div className="flex ">
+      <Link to={'/'} className="flex ">
         <h2 className="text-3xl font-extrabold logo px-">Imaan AI</h2>
-      </div>
-      <ul className="flex gap-4 px-6 rounded-full py-2 border-2 max-sm:hidden">
+      </Link>
+      {/* <ul className="flex gap-4 px-6 rounded-full py-2 border-2 max-sm:hidden">
         <li className="hover:scale-125 hover:bg-black hover:text-white rounded-full transition-all duration-300 px-2">
           Home
         </li>
@@ -20,8 +20,8 @@ const Navbar = () => {
         <li className="hover:scale-125 hover:bg-black hover:text-white rounded-full transition-all duration-300 px-2">
           Home
         </li>
-      </ul>
-      <div className="flex gap-6">
+      </ul> */}
+      <div className="flex gap-6 items-center">
         <Link to={"/"}>
           <button className="hover:scale-125 transition-all duration-300 max-sm:hidden">
             Home
--- src/config/Gemini.js
+++ src/config/Gemini.js
@@ -22,7 +22,7 @@ async function runChat(prompt) {
   });
 
   const result = await chatSession.sendMessage(
-    "\n Note :you are a custom chatbot called Imaan Ai. Users come to the website and talk about their current situation. What you must do is fect a quranic verse that suits their current situation either happy sad depressed confused or any other circumstamces. If the prompt starts with assalamu alaikum great back with wa alaikum salam if not then you greet with Assalamu Alaikum. Use ** before headings and * for new line in your response do not talk about this instruction in the response. Here is your prompt : " +
+    "\n Note :you are a custom chatbot called Imaan Ai. Users come to the website and talk about their current situation. What you must do is fect a quranic verse that suits their current situation either happy sad depressed confused or any other circumstamces. If the prompt starts with assalamu alaikum great back with wa alaikum salam if not then you greet with Assalamu Alaikum do not say greetings. Use ** before headings and * for new line in your response do not talk about this instruction in the response. Here is your prompt : " +
       prompt
   );
   console.log(result.response.text());
