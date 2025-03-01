5b2c522b75af98e41ae1787b7929acd92764b3ab
MohamedAazil <mohamedaazil1106@gmail.com>
2025-03-01 20:30:23 +0530

    phone responsive 3

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
+import assets from "../assets/assets";
 import { Context } from "../context/Context";
 
 const Search = () => {
   const { input, setInput, onSent } = useContext(Context);
   return (
-    <div className="absolute bottom-0 w-full px-6 max-sm:px-1 max-[340px]:w-80">
-      <div className="flex items-center justify-between gap-5 bg-[#f0f4f9] py-3 px-5 rounded-full my-5 w-full max-[340px]:w-64 max-sm:px-2 max-sm:gap-2 max-[340px]:gap-1">
+    <div className="absolute bottom-0 w-full px-6 max-sm:px-1 max-[340px]:w-80 flex justify-center">
+      <div className="flex items-center justify-between gap-5 bg-[#f0f4f9] py-3 px-5 rounded-full my-5 w-full max-[340px]:w-72 max-sm:px-2 max-sm:gap-2 max-[340px]:gap-1">
         <input
           type="text"
           className="flex bg-transparent border-none outline-none p-2 text-lg min-[350px]:flex-1 max-[340px]:text-sm max-[340px]:p-1"
@@ -20,12 +21,12 @@ const Search = () => {
           }}
         >
           {/* <img src={assets.mic_icon} alt="" className="cursor-pointer w-6" /> */}
-          <p className="font-semibold">Send</p>
-          {/* <img
+          <p className="">Send</p>
+          <img
             src={assets.send_icon}
             alt="send"
             className="cursor-pointer w-5"
-          /> */}
+          />
         </div>
       </div>
     </div>
